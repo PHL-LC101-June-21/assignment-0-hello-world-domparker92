649fddb73836f4994c2ba91955fad0a1f46ec7d2
DomPS <85456740+domparker92@users.noreply.github.com>
2021-06-10 20:50:33 +0000

    Added my personal solution and updated hello.js to print Hello, World!

--- hello.js
+++ hello.js
@@ -1,7 +1,1 @@
-function hello() {
-	// TODO: change this string so that your program prints "Hello world!" 
-	// when you hit the replit run button
-	return "Hello!";
-}
- 
-module.exports = hello;
+console.log("Hello, World!");
